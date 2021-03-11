d3eca338463f3ca031ae7bb014640c89c9742c10
BradleyMaxwell <72202351+BradleyMaxwell@users.noreply.github.com>
2021-03-11 01:26:53 +0000

    Update Perceptron.java

--- Perceptron.java
+++ Perceptron.java
@@ -1,4 +1,16 @@
 
 public class Perceptron {
 	Float[][] data;
+	
+	//methods
+	public Float[][] standardiseData (Float[][] data) {
+		
+		
+		for (int row = 0; row < data.length; row++) {
+			for (int input = 0; input < data[row].length; input++) {
+				
+			}
+		}
+		return data;
+	}
 }
